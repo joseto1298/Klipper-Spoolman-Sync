368574365a6f95b475e50e5a30c68ea7732626e6
joseto1298 <85197083+joseto1298@users.noreply.github.com>
2026-03-06 18:38:10 +0100

    Update request timeouts to 5 seconds
    Increased timeout for requests to Spoolman and Moonraker to 5 seconds.

--- filamentNotice.py
+++ filamentNotice.py
@@ -13,21 +13,20 @@ def main():
         if not moon_url.endswith('/'): moon_url += '/'
         
         fid = sys.argv[1]
-        motivo = sys.argv[2]
 
-        # 1. Petición a Spoolman (Timeout agresivo de 2s)
+        # 1. Petición a Spoolman 5s
         name, mat = "Unknown", "Unknown"
         if fid != "0":
             try:
-                r = requests.get(f"{spool_url.rstrip('/')}/filament/{fid}", timeout=2)
+                r = requests.get(f"{spool_url.rstrip('/')}/filament/{fid}", timeout=5)
                 data = r.json()
                 name, mat = data.get("name"), data.get("material")
             except: pass
 
         # 2. Respuesta única a Moonraker (Abrir el menú)
-        # Enviamos un solo POST con todo para no saturar el bus serie
-        gcode = f'_FILAMENT_INFO ID={fid} NAME="{name}" MATERIAL="{mat}" MOTIVO="{motivo}"'
-        requests.post(f"{moon_url}printer/gcode/script", json={"script": gcode}, timeout=1)
+        # Enviamos un solo POST
+        gcode = f'_FILAMENT_INFO ID={fid} NAME="{name}" MATERIAL="{mat}"'
+        requests.post(f"{moon_url}printer/gcode/script", json={"script": gcode}, timeout=5)
 
     except:
         sys.exit(1)
